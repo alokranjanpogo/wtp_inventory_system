076a876b00dae0260413bd5dd851ba712c39af7f
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-11 17:23:36 +0530

    Update 4_LRD_Recommendation.py

--- pages/4_LRD_Recommendation.py
+++ pages/4_LRD_Recommendation.py
@@ -1,1 +1,22 @@
+import streamlit as st
 
+from utils.lrd_engine import (
+    get_recommended_dose
+)
+
+st.title(
+    "LRD Recommendation"
+)
+
+turbidity = st.number_input(
+    "Enter Turbidity",
+    min_value=0.0,
+    value=20.0
+)
+
+if st.button(
+        "Get Recommendation"
+):
+
+    result = get_recommended_dose(
+        turbidity
