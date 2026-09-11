84eb3ad70d080c51353a14667fafe33d6b37d54e
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-11 17:20:32 +0530

    Update inventory_engine.py

--- utils/inventory_engine.py
+++ utils/inventory_engine.py
@@ -1,1 +1,35 @@
+def calculate_days_remaining(
+        current_stock,
+        avg_daily_consumption
+):
 
+    if avg_daily_consumption == 0:
+        return 0
+
+    return round(
+        current_stock /
+        avg_daily_consumption,
+        1
+    )
+
+
+def calculate_reorder_point(
+        lead_time,
+        avg_daily_consumption
+):
+
+    return (
+        lead_time *
+        avg_daily_consumption
+    )
+
+
+def calculate_safety_stock(
+        avg_daily_consumption,
+        safety_days=7
+):
+
+    return (
+        avg_daily_consumption *
+        safety_days
+    )
