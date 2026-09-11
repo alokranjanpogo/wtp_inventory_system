88c7a9bf3b31094fdbafe0718e825e01386d303a
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-11 17:23:10 +0530

    Update 3_Chemical_Analytics.py

--- pages/3_Chemical_Analytics.py
+++ pages/3_Chemical_Analytics.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+from utils.data_loader import (
+    load_chemical_consumption
+)
+
+df = load_chemical_consumption()
+
+st.title(
+    "Chemical Analytics"
+)
+
+st.write(df.head())
+
+st.write(df.shape)
