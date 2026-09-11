e76bbe8620a0daf6f4dc0ab78ee25dbd1ff8da04
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-11 17:21:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="WTP Predictive Chemical Management",
+    layout="wide"
+)
+
+st.title(
+    "WTP Predictive Chemical Management System"
+)
+
+st.markdown("""
+### Welcome
+
+This system is designed to analyze:
+
+- Daily Turbidity Data_Pc.xlsx
+- Monthly Historical Turbidity_Pc.xlsx
+- Chemical_consumption_data_Pc.xlsx
+- Current_SCC_Pc.xlsx
+- LRD_Pc.xlsx
+
+Use the pages on the
